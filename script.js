e9bae3a369501bb92477d1290865e918214c8850
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-20 14:54:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+    // Write your code here
+    const lowerS1 = s1.toLowerCase();
+    const lowerS2 = s2.toLowerCase();
+
+    return lowerS1.indexOf(lowerS2);
 }
 
 // Please do not change the code below
